61321c4fd130b6b0367648be5e42f5ee4396089a
MortenSkildpadd3 <at114758@edu.aarhustech.dk>
2024-02-21 09:12:51 +0100

    Added function Dashboard

--- sketch.js
+++ sketch.js
@@ -1,6 +1,29 @@
-function setup() {
-  createCanvas(350, 550);
-  background(220);
+
+function Dashboard(){
+  function setup() {
+    createCanvas(350, 550);
+    background(200);
+    
+    MorgenmadKnap = createInput();
+    MorgenmadKnap.position(50,50);
+    MorgenmadKnap.size(50)
+    
+
+    FrokostKnap = createButton(Frokost)
+    FrokostKnap.position()
+
+    AftensmadKnap = createButton(Aftensmad)
+    AftensmadKnap.position()
+
+    SnackKnap = createButton(Snack)
+    SnackKnap.position()
+
+    BMIKnap = createButton(BMI)
+    BMIKnap.position()
+
+    OpskrifterKnap = createButton(Opskrifter)
+    OpskrifterKnap.position()
+  }
 }
 
 function opskrifter(){
