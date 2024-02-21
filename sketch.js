f467efe2c3d2aec0c46bcce23f471698795c31ba
soer376d <soer376d@edu.aarhustech.dk>
2024-02-21 09:45:30 +0100

    Tilføjet AftensmadUI

--- sketch.js
+++ sketch.js
@@ -264,3 +264,91 @@ function MorgenmadUI(){
       
     }
     }
+
+    function AftensmadUI(){
+
+      function setup() {
+        createCanvas(350, 550);
+        background('#f0f0f0');
+        
+        textSize(26);
+        textAlign(CENTER);
+        fill('#333');
+        text("Aftensmad", width / 2, 50);
+      
+        
+        skyrButton = createButton('Kartofler');
+        skyrButton.position(25, 95);
+        skyrButton.size(290, 40);
+        skyrButton.mousePressed();
+        skyrButton.style('background-color', 'yellow');
+        skyrButton.style('color', 'black');
+        skyrButton.style('border', 'none');
+        skyrButton.style('border-radius', '5px');
+        
+        BananButton = createButton('Broccoli');
+        BananButton.position(25, 145);
+        BananButton.size(290, 40);
+        BananButton.mousePressed();
+        BananButton.style('background-color', 'yellow');
+        BananButton.style('color', 'black');
+        BananButton.style('border', 'none');
+        BananButton.style('border-radius', '5px');
+        
+        BollerButton = createButton('Kylling');
+        BollerButton.position(25, 195);
+        BollerButton.size(290, 40);
+        BollerButton.mousePressed();
+        BollerButton.style('background-color', 'yellow');
+        BollerButton.style('color', 'black');
+        BollerButton.style('border', 'none');
+        BollerButton.style('border-radius', '5px');
+        
+        SmoothieButton = createButton('Kylling, Ris og Broccoli');
+        SmoothieButton.position(25, 245);
+        SmoothieButton.size(290, 40);
+        SmoothieButton.mousePressed();
+        SmoothieButton.style('background-color', 'green');
+        SmoothieButton.style('color', 'black');
+        SmoothieButton.style('border', 'none');
+        SmoothieButton.style('border-radius', '5px');
+        
+        AndetButton = createButton('Andet');
+        AndetButton.position(25, 295);
+        AndetButton.size(290, 40);
+        AndetButton.mousePressed();
+        AndetButton.style('background-color', 'red');
+        AndetButton.style('color', 'black');
+        AndetButton.style('border', 'none');
+        AndetButton.style('border-radius', '5px');
+      
+        AndetButton = createButton('Cancel');
+        AndetButton.position(15, 395);
+        AndetButton.size(150, 40);
+        AndetButton.mousePressed();
+        AndetButton.style('background-color', 'red');
+        AndetButton.style('color', 'black');
+        AndetButton.style('border', 'none');
+        AndetButton.style('border-radius', '5px');
+        
+        AndetButton = createButton('Tilføj');
+        AndetButton.position(180, 395);
+        AndetButton.size(150, 40);
+        AndetButton.mousePressed();
+        AndetButton.style('background-color', 'green');
+        AndetButton.style('color', 'black');
+        AndetButton.style('border', 'none');
+        AndetButton.style('border-radius', '5px');
+      
+        resultText = createElement('textarea', "Antal kcal:");
+        resultText.position(30, 450);
+        resultText.size(290, 80);
+        resultText.attribute('readonly', 'true');
+        resultText.style('font-size', '18px');
+        resultText.style('background-color', '#ffffff');
+        resultText.style('border', 'none');
+        resultText.style('border-radius', '5px');
+        resultText.style('resize', 'none');
+        
+      }
+      }
